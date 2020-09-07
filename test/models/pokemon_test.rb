45bc4cecc8d6838daf0a99f5445f78aed081891f
David Legare <davidlegare01@gmail.com>
2020-09-07 11:58:31 -0400

    show test

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
 
+  test "should reach existing pokemon" do
+    mon = Pokemon.find(1)
+    assert !mon.nil?, true
+  end
+
   test "should create new pokemon" do
     mon = Pokemon.new(:identifier => 2, :name => "Ivysaur", :type_1 => "Grass", :type_2 => "Poison", :total => 405, :hp => 60,
                       :attack => 62, :defense => 63, :sp_attack => 80, "sp_defense" => 80, "speed" => 60, "generation" => 1, "legendary" => "False")
